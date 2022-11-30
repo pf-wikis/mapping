4377d8bea2545f8f3457de18046eebc1ead16fe6
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2022-11-30 15:15:16 +0100

    Smoother continent_buffer

--- tile-compiler/src/main/java/io/github/pfwikis/layercompiler/steps/AddContinentsBuffer.java
+++ tile-compiler/src/main/java/io/github/pfwikis/layercompiler/steps/AddContinentsBuffer.java
@@ -10,12 +10,17 @@ public class AddContinentsBuffer extends LCStep {
     @Override
     public byte[] process(Ctx ctx, byte[] f) throws IOException {
         var buffered = Tools.qgis("native:buffer", f,
-            "--DISTANCE=1",
+            "--DISTANCE=0.5",
             "--SEGMENTS=20",
             "--END_CAP_STYLE=0", "--JOIN_STYLE=0", "--MITER_LIMIT=2",
             "--DISSOLVE=true");
-        var reduced = Tools.mapshaper(buffered, "-filter-fields", "-explode");
-        new LayerCompiler(new Ctx("continents_buffer", ctx.getOptions(), ctx.getGeo(), reduced)).compile();
+        var reduced = Tools.mapshaper(buffered, "-dissolve", "-filter-fields", "-explode", "-simplify", "percentage=0.3", "keep-shapes");
+        var smooth = Tools.qgis("native:smoothgeometry", reduced,
+            "--ITERATIONS=3",
+            "--OFFSET=0.3",
+            "--MAX_ANGLE=180"
+        );
+        new LayerCompiler(new Ctx("continents_buffer", ctx.getOptions(), ctx.getGeo(), smooth)).compile();
         return f;
     }
 
