66f4c0f467b3c035a2afdb61f848f32a7c404ae4
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2023-07-18 14:26:14 +0200

    Update compiling container

--- .github/compiler-docker/compile.sh
+++ .github/compiler-docker/compile.sh
@@ -8,4 +8,5 @@ printf "VITE_MAX_ZOOM=$maxzoom\nVITE_DATA_PATH=$datapath" > .env.local
 npm ci
 npm run build
 cd ../tile-compiler
-mvn -B compile exec:java -Dexec.args="compileTiles -maxZoom $maxzoom -useBuildShortcut -dataPath $datapath -prodDetail"
+mvn -B compile package
+java -jar target/tile-compiler.jar compileTiles -maxZoom $maxzoom -useBuildShortcut -dataPath $datapath -prodDetail
