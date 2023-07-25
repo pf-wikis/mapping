cf5671232af7175a199cbb95f0138908afe63f27
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2023-07-25 10:48:53 +0200

    Update diff.sh

--- .github/compiler-docker/diff.sh
+++ .github/compiler-docker/diff.sh
@@ -13,10 +13,10 @@ for new in head/sources/*.gpkg; do
 
 
         if grep -q "\"type\": \"LineString\"" "$oldJ" || grep -q "\"type\": \"MultiLineString\"" "$oldJ" ; then
-            mv "$oldJ" "unbuffered.$oldJ"
-            mv "$newJ" "unbuffered.$newJ"
-            qgis_process run native:buffer --distance_units=meters --area_units=m2 --ellipsoid=EPSG:7030 "--INPUT='unbuffered.$oldJ'" --DISTANCE='expression:"width" *0.000005' --SEGMENTS=5 --END_CAP_STYLE=0 --JOIN_STYLE=0 --MITER_LIMIT=2 --DISSOLVE=false "--OUTPUT='$oldJ'"
-            qgis_process run native:buffer --distance_units=meters --area_units=m2 --ellipsoid=EPSG:7030 "--INPUT='unbuffered.$newJ'" --DISTANCE='expression:"width" *0.000005' --SEGMENTS=5 --END_CAP_STYLE=0 --JOIN_STYLE=0 --MITER_LIMIT=2 --DISSOLVE=false "--OUTPUT='$newJ'"
+            mv "$oldJ" "$oldJ.unbuf.geojson"
+            mv "$newJ" "$newJ.unbuf.geojson"
+            qgis_process run native:buffer --distance_units=meters --area_units=m2 --ellipsoid=EPSG:7030 "--INPUT='$oldJ.unbuf.geojson'" --DISTANCE='expression:"width" *0.000005' --SEGMENTS=5 --END_CAP_STYLE=0 --JOIN_STYLE=0 --MITER_LIMIT=2 --DISSOLVE=false "--OUTPUT='$oldJ'"
+            qgis_process run native:buffer --distance_units=meters --area_units=m2 --ellipsoid=EPSG:7030 "--INPUT='$newJ.unbuf.geojson'" --DISTANCE='expression:"width" *0.000005' --SEGMENTS=5 --END_CAP_STYLE=0 --JOIN_STYLE=0 --MITER_LIMIT=2 --DISSOLVE=false "--OUTPUT='$newJ'"
         fi
 
 
