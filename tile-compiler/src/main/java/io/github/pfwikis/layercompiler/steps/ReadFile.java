3e57f6ed52725ae8e03d10f1a50428cffb942c43
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2023-10-31 16:26:55 +0100

    fix read_file step

--- tile-compiler/src/main/java/io/github/pfwikis/layercompiler/steps/ReadFile.java
+++ tile-compiler/src/main/java/io/github/pfwikis/layercompiler/steps/ReadFile.java
@@ -3,26 +3,37 @@ package io.github.pfwikis.layercompiler.steps;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Objects;
 
 import org.apache.commons.io.FileUtils;
 
 import io.github.pfwikis.run.Tools;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class ReadFile extends LCStep {
 
     private final File file;
     private final String layer;
 
-    @Override
-    public byte[] process() throws IOException {
-        var finalFile = Path.of("../sources").resolve(file.toPath()).toFile().getCanonicalFile();
+    
+    public ReadFile(File file, String layer) {
+		this.file = file;
+		this.layer = layer;
+		
+		if(file == null) {
+			Objects.requireNonNull(layer, "Either file or layer must not be null for READ_FILE.");
+		}
+	}
 
-        if(finalFile.getName().endsWith(".gpkg")) {
+
+	@Override
+    public byte[] process() throws IOException {
+		if(file != null) {
+			var finalFile = Path.of("../sources").resolve(file.toPath()).toFile().getCanonicalFile();
+			return FileUtils.readFileToByteArray(finalFile);
+		}
+		else {
             return Tools.ogr2ogr(ctx.getMappingDataFile().toString(), "-dim", "XY", "-mapFieldType", "DateTime=String", layer);
         }
-        return FileUtils.readFileToByteArray(finalFile);
     }
 
 }
