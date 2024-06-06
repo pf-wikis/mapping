b413086b5c0c6350d0beed55016560d390135885
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2024-06-06 13:30:12 +0200

    Add graveyard icon

--- tile-compiler/src/main/java/io/github/pfwikis/layercompiler/steps/LocationTypeToIcon.java
+++ tile-compiler/src/main/java/io/github/pfwikis/layercompiler/steps/LocationTypeToIcon.java
@@ -3,11 +3,17 @@ package io.github.pfwikis.layercompiler.steps;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset.Entry;
 
 import io.github.pfwikis.layercompiler.steps.model.LCContent;
 import io.github.pfwikis.layercompiler.steps.model.LCStep;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class LocationTypeToIcon extends LCStep {
 
     @Override
@@ -17,8 +23,10 @@ public class LocationTypeToIcon extends LCStep {
     		.map(n->n.substring(9, n.length()-4))
     		.collect(Collectors.toSet());
     	
+    	var counts = HashMultiset.<String>create();
     	var fc = getInput().toFeatureCollection();
     	fc.getFeatures().forEach(f-> {
+    		counts.add(f.getProperties().getType());
     		if(icons.contains(f.getProperties().getType())) {
     			f.getProperties().setIcon("location-"+f.getProperties().getType());
     		}
@@ -27,6 +35,16 @@ public class LocationTypeToIcon extends LCStep {
     		}
     		f.getProperties().setType(null);
     	});
+    	
+    	log.info(
+			"Counted location types:\n{}",
+	    	counts.entrySet()
+	    		.stream()
+	    		.sorted(Comparator.<Entry<String>, Integer>comparing(Entry::getCount).reversed())
+	    		.map(e->e.getElement()+"\tx"+e.getCount())
+	    		.collect(Collectors.joining("\n"))
+		);
+    	
     	return LCContent.from(fc);
     }
 }
