65a29aac1c980699dd091e7fe44f996b5d20a8c5
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2024-06-23 23:30:54 +0200

    Fix map waiting endlessly for DB connection

--- frontend/src/CachedPmTiles.ts
+++ frontend/src/CachedPmTiles.ts
@@ -1,4 +1,4 @@
-import { DBSchema, openDB, deleteDB } from "idb";
+import { DBSchema, openDB, deleteDB, IDBPDatabase } from "idb";
 import { FetchSource, RangeResponse, Source } from "pmtiles";
 
 const buildId = parseInt(import.meta.env.VITE_DATA_HASH)||0;
@@ -13,11 +13,11 @@ interface Schema extends DBSchema {
   };
 }
 
-const db = await openDB<Schema>(dbId, 1, {
+const dbPromise=openDB<Schema>(dbId, 1, {
   upgrade(database, oldVersion, newVersion, transaction, event) {
     database.createObjectStore(ranges)
   },
-})
+});
 
 setTimeout(async ()=>{
   let databases = await indexedDB.databases()
@@ -28,21 +28,30 @@ setTimeout(async ()=>{
   })
 }, 5000);
 
+
 export class CachedSource implements Source {
   fetcher: FetchSource;
 
   constructor(url:string) {
     this.fetcher = new FetchSource(url);
-    //if there is no number buildId disable loading from cache (for dev purposes)
-    if(!buildId)
-      this.getBytes = this.load;
+    this.getBytes = this.load; //at first we just redirect loads to the fetcher
+
+    dbPromise.then(db => {
+      //as soon as the cache runs we want to load and store
+      this.getBytes = this.load = (offset: number, length: number, signal?: AbortSignal, etag?: string) => this.loadAndStore(db, offset, length, signal, etag);
+      //and we want to try loading from the cache first, if we are not in a DEV scenario
+      if(buildId)
+        this.getBytes = (offset: number, length: number, signal?: AbortSignal, etag?: string) => this.loadCachedOrFresh(db, offset, length, signal, etag);
+    });
   }
+
+  getBytes: (offset: number, length: number, signal?: AbortSignal, etag?: string) => Promise<RangeResponse>;
 
   key(offset: number, length: number):Key {
     return [offset, length]
   }
 
-  getBytes(offset: number, length: number, signal?: AbortSignal, etag?: string):Promise<RangeResponse> {
+  loadCachedOrFresh(db:IDBPDatabase<Schema>, offset: number, length: number, signal?: AbortSignal, etag?: string):Promise<RangeResponse> {
     return db.get(ranges, this.key(offset, length))
       .catch(e=>this.load(offset, length, signal, etag))
       .then(content=>{
@@ -52,11 +61,15 @@ export class CachedSource implements Source {
       });
   }
 
-  load(offset: number, length: number, signal?: AbortSignal, etag?: string):Promise<RangeResponse> {
+  loadAndStore(db:IDBPDatabase<Schema>, offset: number, length: number, signal?: AbortSignal, etag?: string):Promise<RangeResponse> {
     return this.fetcher.getBytes(offset, length, signal, etag).then(resp => {
       db.put(ranges, resp.data, this.key(offset, length));
       return resp;
     });
+  }
+
+  load(offset: number, length: number, signal?: AbortSignal, etag?: string):Promise<RangeResponse> {
+    return this.fetcher.getBytes(offset, length, signal, etag);
   }
 
   getKey() {
